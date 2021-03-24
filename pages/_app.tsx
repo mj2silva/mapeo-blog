3e8c38892c5bce86a9ffc97b324478bdf0b67411
Manuel Silva <manuel.silvag1@gmail.com>
2021-03-24 15:54:05 -0500

    Página de error 404

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,11 @@ function MyApp({ Component, pageProps } : AppProps) : ReactElement {
   const router = useRouter();
   const { asPath } = router;
   const isInternal = asPath.includes('/internal/');
-  return (
+  return (Component.displayName !== 'Error 404') ? (
     <Layout isPublic={!isInternal}>
       <Component {...pageProps} />
     </Layout>
-  );
+  ) : <Component {...pageProps} />;
 }
 
 export default MyApp;
